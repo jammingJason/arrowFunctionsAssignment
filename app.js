c61f088973e910cea96afc82ed261aae5d5a057d
Van Jason Crews <vanjasoncrews@gmail.com>
2022-07-07 13:35:20 -0400

    completed js file

---
+++ app.js
@@ -0,0 +1,10 @@
+// function double(arr) {
+// 	return arr.map(function(val) {
+// 		return val * 2;
+// 	});
+// }
+
+const double = (arr) => arr.map((val) => val * 2);
+
+const squareAndFindEvens = (numbers) => numbers.map((num) => num ** 2).filter((square) => square % 2 === 0);
+// const squareAndFindEvens = (numbers) => numbers.map((val) => val ** 2).filter((square) => square % 2 === 0);
